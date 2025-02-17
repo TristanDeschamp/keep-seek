99dbdb402940503ec2d39013911b3cad5cf53238
Tristan Deschamp <tristan2020.d@gmail.com>
2025-02-17 14:50:40 +0100

    3

--- vite.config.js
+++ vite.config.js
@@ -23,12 +23,12 @@ export default defineConfig({
         theme_color: "#3B82F6",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-512x512.png",
+            src: "512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
